02beb33ba3ff742a952584e5a0855d7c768604c6
Renan Trajano <128718991+RenanTC007@users.noreply.github.com>
2025-05-19 17:32:29 -0300

    Delete atualizado 21/spolbp2_cards-main directory

--- atualizado 21/spolbp2_cards-main/src/br/edu/ifsp/spo/java/cards/itens/Naipe.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.spo.java.cards.itens;
-
-public enum Naipe {
-    COPAS, OUROS, PAUS, ESPADAS;
-}
--- atualizado 21/spolbp2_cards-main/src/br/edu/ifsp/spo/java/cards/itens/Valor.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.spo.java.cards.itens;
-
-public enum Valor {
-    AS, DOIS, TRES, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, DEZ, DAMA, VALETE, REI;
-}
